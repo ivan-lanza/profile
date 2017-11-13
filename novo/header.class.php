de8c7665f4842f2d8d145ea1ae04b7e3e2801967
ivan-lanza <ivan@flatpixel.com.br>
2017-11-13 13:00:56 -0200

    Versão 2.0 - Novo Header

---
+++ novo/header.class.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Hello, world!</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="col-12 pattern-header">
+        <img src="imgs/logo-profile.png" class="img-fluid mt-2 mb-2  ml-5">
+      </div>
+    </div>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto ml-5">
+      <li class="nav-item">
+        <a class="nav-link" href="#">HOME</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">QUEM SOMOS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">CASTING</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">PORTIFOLIO</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">CONTATO</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0 mr-5">
+      <ul class="navbar-nav mr-auto ml-5">
+        <li class="nav-item">
+          <a class="nav-link" href="#"><img src="imgs/face.png"></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><img src="imgs/insta.png"></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><img src="imgs/tw.png"></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><img src="imgs/yt.png"></a>
+        </li>
+      </ul>
+    </form>
+  </div>
+</nav>
+<div class="container">
+  <img src="imgs/slider.jpg" class="img-fluid col-12">
+</div>
+  </body>
+</html>
